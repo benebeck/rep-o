b5a2921f8345ed705064b9bacaaa584251252829
Benedikt Beckmann <benedikt.beckmann@embl.de>
2012-06-20 10:51:06 +0200

    Initial Commit

---
+++ TH/THAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THAppDelegate.h
+//  TH
+//
+//  Created by Benedikt Beckmann on 20.06.12.
+//  Copyright (c) 2012 BioApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TH/THViewController.h
@@ -0,0 +1,13 @@
+//
+//  THViewController.h
+//  TH
+//
+//  Created by Benedikt Beckmann on 20.06.12.
+//  Copyright (c) 2012 BioApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THViewController : UIViewController
+
+@end
