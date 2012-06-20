9e9fb2934f7e105b8d67c2dc644cee9e89cd25f9
Benedikt Beckmann <benedikt.beckmann@embl.de>
2012-06-20 10:29:40 +0200

    initial commit

---
+++ TexasHoldem/TexasHoldem/TexasHoldemAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TexasHoldemAppDelegate.h
+//  TexasHoldem
+//
+//  Created by Benedikt Beckmann on 20.06.12.
+//  Copyright (c) 2012 BioApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TexasHoldemAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
