fd5d3b01ec8b3e33c28b2ea704604160c87c2406
Benedikt Beckmann <benedikt.beckmann@embl.de>
2012-06-20 10:39:14 +0200

    Initial Commit

---
+++ TH/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TH
+//
+//  Created by Benedikt Beckmann on 20.06.12.
+//  Copyright (c) 2012 BioApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THAppDelegate class]));
+    }
+}
